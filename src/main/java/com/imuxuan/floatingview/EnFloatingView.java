6c2d9eaa41ba1a8362351f3207ba046cc8c6bc6f
zhuangdaoyuan <zy6576753>
2020-01-19 14:36:22 +0800

    FloatView库,Camera库

---
+++ src/main/java/com/imuxuan/floatingview/EnFloatingView.java
@@ -0,0 +1,31 @@
+package com.imuxuan.floatingview;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+
+/**
+ * @ClassName EnFloatingView
+ * @Description 悬浮窗
+ * @Author Yunpeng Li
+ * @Creation 2018/3/15 下午5:04
+ * @Mender Yunpeng Li
+ * @Modification 2018/3/15 下午5:04
+ */
+public class EnFloatingView extends FloatingMagnetView {
+
+    private final ImageView mIcon;
+
+    public EnFloatingView(@NonNull Context context) {
+        super(context, null);
+        inflate(context, R.layout.en_floating_view, this);
+        mIcon = findViewById(R.id.icon);
+    }
+
+    public void setIconImage(@DrawableRes int resId){
+        mIcon.setImageResource(resId);
+    }
+
+}
